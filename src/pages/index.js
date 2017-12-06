44f908321e98e9c2de9ed08314f159d6465873b7
Reynaldo Abreu <reynaldo.abreu@paysafe.com>
2017-12-06 13:29:55 +0000

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>This is a Website</h1>
+    <p>There are many like this, but this one is mine.</p>
+    <p>We are still learning or ways around this.</p>
+    <img src="http://lorempixel.com/400/200" alt="an image" />
+    <blockquote>
+      Lorem ipsum dolor sit amet, at probatus corrumpit mea. Feugait civibus sea ne, ex est detracto erroribus laboramus, sed te volumus omittam prodesset. Nam voluptua appareat efficiantur ne. Duo in sumo prima, harum choro vulputate in vis. Sea eu vide perfecto.
+    </blockquote>
+    <h2>Some links to look at:</h2>
+    <ul>
+      <li>
+        <Link to="/page-2/">Go to page 2</Link>
+      </li>
+    </ul>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>Greetings, what is your name traveller?</h1>
+    <p>Mei legere delicata te, id vel vocent iisque intellegebat, sed prompta scribentur eu. Pri quaestio iracundia in, omnis recusabo cu mei. Id mea graeci probatus, eam in quot primis legimus. Probo voluptua vim ea. Vim alienum evertitur expetendis at, id ius eros nihil doming. In dolores perfecto per.</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default SecondPage
