b4e82aade0a99364e40879279682350ec2e5318d
Reynaldo Abreu <reynaldo.abreu@paysafe.com>
2018-07-24 13:09:55 +0100

    fixed vulnerabilities

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,51 +7,22 @@ import './index.css'
 
 const Header = () => (
   <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+    style={{ background: 'rebeccapurple', marginBottom: '1.45rem', }}>
+    <div style={{ margin: '0 auto', maxWidth: 960, padding: '1.45rem 1.0875rem', }}    >
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          To Blog or not to, that is the Question
+        <Link to="/" style={{ color: 'white', textDecoration: 'none', }}>
+          The data consuming blog
         </Link>
       </h1>
     </div>
   </div>
 )
 
-const TemplateWrapper = ({ children }) => (
+const TemplateWrapper = ({ children, data }) => (
   <div>
-    <Helmet
-      title="A Sample blog"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
+    <Helmet title="{data.site.siteMetadata.title}" meta={[{ name: 'description', content: 'Sample' }, { name: 'keywords', content: 'sample, something' },]} />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div style={{ margin: '0 auto', maxWidth: 960, padding: '0px 1.0875rem 1.45rem', paddingTop: 0, }}    >
       {children()}
     </div>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
+const IndexPage = () =>
   <div>
     <h1>This is a Website</h1>
     <p>There are many like this, but this one is mine.</p>
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <blockquote>
       Lorem ipsum dolor sit amet, at probatus corrumpit mea. Feugait civibus sea ne, ex est detracto erroribus laboramus, sed te volumus omittam prodesset. Nam voluptua appareat efficiantur ne. Duo in sumo prima, harum choro vulputate in vis. Sea eu vide perfecto.
     </blockquote>
-    <p>Just showing off stuff to Mark</p>
+    <p>Dicam partem dictas eu per, te vis laboramus definiebas posidonium, eu vis modus audiam. Sale luptatum verterem ea ius, clita dolorem cum an. At cum eruditi eleifend definitionem, ad mucius qualisque has. Id vix equidem fierent, duo ne sint conclusionemque, cu postea dicunt per. Eu quo mutat ludus invenire, sit postea efficiantur eu. In sed bonorum blandit.</p>
     <h2>Some links to look at:</h2>
     <ul>
       <li>
@@ -18,6 +18,5 @@ const IndexPage = () => (
       </li>
     </ul>
   </div>
-)
 
 export default IndexPage
